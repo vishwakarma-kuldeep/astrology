f8e2d9e8c7b723a46cf75b9770567dce58e5054f
Kuldeep-Kumar <kuldeep.solutions900@gmail.com>
2023-06-12 18:31:21 +0530

    gallery video count added

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,20 +1,48 @@
-const Gallery = require('../controllers/gallery');
-const { authenticateToken } = require('../middlewares/auth');
-const { checkAdmin } = require('../middlewares/adminmiddleware');
-const { checkFolder, checkValidvideo } = require('../middlewares/gallery');
-const {checkValidSubscription} = require('../middlewares/subscription');
-const { checkUser } = require('../middlewares/user');
-const router = require('express').Router();
+const Gallery = require('../controllers/gallery')
+const { authenticateToken } = require('../middlewares/auth')
+const { checkAdmin } = require('../middlewares/adminmiddleware')
+const { checkFolder, checkValidvideo } = require('../middlewares/gallery')
+const { checkValidSubscription } = require('../middlewares/subscription')
+const { checkUser } = require('../middlewares/user')
+const router = require('express').Router()
 
-router.post('/create',[authenticateToken,checkAdmin],Gallery.addFolder);
-router.get('/get',[authenticateToken],Gallery.getFolders);
+router.post('/create', [authenticateToken, checkAdmin], Gallery.addFolder)
+router.get('/get', [authenticateToken], Gallery.getFolders)
 
-router.post('/update/:id',[authenticateToken,checkAdmin,checkFolder],Gallery.updateFolder);
-router.post('/delete/:id',[authenticateToken,checkAdmin,checkFolder],Gallery.deleteFolder);
-router.post('/add-video/:id',[authenticateToken,checkAdmin,checkFolder],Gallery.addVideo);
-router.post('/delete-video/:id',[authenticateToken,checkAdmin,checkValidvideo],Gallery.deleteVideo);
-router.post('/update-video/:id',[authenticateToken,checkAdmin,checkValidvideo],Gallery.updateVideo);
+router.post(
+  '/update/:id',
+  [authenticateToken, checkAdmin, checkFolder],
+  Gallery.updateFolder,
+)
+router.post(
+  '/delete/:id',
+  [authenticateToken, checkAdmin, checkFolder],
+  Gallery.deleteFolder,
+)
+router.post(
+  '/add-video/:id',
+  [authenticateToken, checkAdmin, checkFolder],
+  Gallery.addVideo,
+)
+router.post(
+  '/delete-video/:id',
+  [authenticateToken, checkAdmin, checkValidvideo],
+  Gallery.deleteVideo,
+)
+router.post(
+  '/update-video/:id',
+  [authenticateToken, checkAdmin, checkValidvideo],
+  Gallery.updateVideo,
+)
 
-router.get('/get-videos/:id',[authenticateToken,checkUser,checkFolder,checkValidSubscription],Gallery.getVideos);
-router.get('/get-video/:id',[authenticateToken,checkUser,checkValidvideo,checkValidSubscription],Gallery.getVideo);
-module.exports = router;
+router.get(
+  '/get-videos/:id',
+  [authenticateToken, checkUser, checkFolder, checkValidSubscription],
+  Gallery.getVideos,
+)
+router.get(
+  '/get-video/:id',
+  [authenticateToken, checkUser, checkValidvideo, checkValidSubscription],
+  Gallery.getVideo,
+)
+module.exports = router
